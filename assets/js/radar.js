6c2432520d70c9e981693fdd2c8c6e0b29813966
1phantm1 <pietrodavila37@gmail.com>
2024-12-13 11:54:14 -0300

    save

--- assets/js/radar.js
+++ assets/js/radar.js
@@ -4,33 +4,34 @@ const data = {
         "HTML",
         "CSS",
         "Python",
+        "React.js",
         "PHP",
         "MySQL",
         "Javascript",
-        "Pentesting"
+        "C++",
+        "Pentesting",
+        
     ],
     datasets: [{
         label: "Minhas Habilidades",
-        data: [90, 90, 90, 75, 75, 70, 50],
-        fill: true,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgb(255, 99, 132)',
-        pointBackgroundColor: 'rgb(255, 99, 132)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgb(255, 99, 132)'
+        data: [90, 90, 90, 80, 75, 75, 70, 65, 50],
+        backgroundColor: [
+            '#e44c27',
+            '#0171be', 
+            '#4b8abc', 
+            '#243b6f',
+            '#777bb3', 
+            '#43b7fe', 
+            '#d6bb33', 
+            '#004580',
+            '#cd1f24', 
+          ],
+
     }]
 }
 const config = {
-    type: "radar",
+    type: "doughnut",
     data: data,
-    options: {
-        elements: {
-            line: {
-                borderWidth: 3
-            }
-        }
-    },
 }
 
 let ctx = document.getElementById("skillRadar")
